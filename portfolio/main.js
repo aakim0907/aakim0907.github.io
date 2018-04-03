6c2e604f784fef979386c19c59f98e67e1dcb857
Young Eun Kim <aakim0907@gmail.com>
2018-04-02 22:46:26 -0400

    update nav bar, main banner, project overlay

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,20 @@
-// Navbar active
+// Navbar display active
 $('ul li a').click(function() { 
   $('li a').removeClass("active"); 
   $(this).addClass("active"); 
 });
+
+// Hide and show navbar
+$(document).ready(function () {
+  $(".navbar").hide();
+
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 750) {
+        $(".navbar").fadeIn(400);
+      } else {
+        $(".navbar").fadeOut(400);
+      }
+    });
+  });
+})
