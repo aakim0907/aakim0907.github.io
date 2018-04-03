f627f5b56a48999477d52acde54e6ff153c4cf37
Young Eun Kim <aakim0907@gmail.com>
2018-04-02 23:55:15 -0400

    update images, project hover content

--- portfolio/main.js
+++ portfolio/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
   $(function () {
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 750) {
+      if ($(this).scrollTop() > 500) {
         $(".navbar").fadeIn(400);
       } else {
         $(".navbar").fadeOut(400);
